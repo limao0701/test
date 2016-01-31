18f5080d6dd8e34aec96042ea881f4b7bf83e3e8
limao0701 <limao0701@gmail.com>
2016-01-31 23:16:44 +0800

    led flush ok

--- main.c
+++ main.c
@@ -8,6 +8,7 @@
 #define LED2ON (0X01<<6)
 #define LED3ON (0X01<<7)
 #define LED4ON (0X01<<8)
+void delay();
 int main(void){
 	GPBCON &=~(255<<10);
 	GPBCON |= LED1;
@@ -18,6 +19,22 @@ int main(void){
 	GPBDAT |=LED1ON;
 	GPBDAT |=LED2ON;
 	GPBDAT |=LED3ON;
-	while(1);
+	while(1){
+		GPBDAT &=~(7<<5);
+		delay();
+		GPBDAT |=(LED1ON | LED2ON);
+    delay();
+	}
 	return 0;
+}
+
+
+void delay(){
+	int i=3000000;
+	int j=1000000;
+	int c=0;
+	for (;i>0;i--){
+		for(;j>0;j--)
+		   c=j>i;
+	}
 }
